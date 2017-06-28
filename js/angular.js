7194868febdc2395d475088ff2df80a066a806ad
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-06-29 10:06:14 +1200

    fix alogirthm and add ability to change requency

--- js/angular.js
+++ js/angular.js
@@ -1,13 +1,10 @@
 var mortgageApp = angular.module('mortgageApp', []);
 
 mortgageApp.controller('mainController', function($scope) {
-	$scope.housePrice = 0
-	$scope.deposit = 0
-	$scope.loanLength = 0
-	$scope.interest = 0
 
 	$scope.calcRepayment = function (housePrice, deposit, loanLength, interest, term) {
-		r = (interest/100)/loanLength;
+		console.log(housePrice, deposit, loanLength, interest, term)
+		r = (interest/100)/term;
 	    n = loanLength * term;
 	    p = housePrice - deposit;
 	    part1 = Math.pow((1+r), -n);
@@ -15,4 +12,22 @@ mortgageApp.controller('mainController', function($scope) {
 	    repayment = Math.round(repayment);
 	    return repayment
 	}
+
+	$scope.options = [
+	        {
+	          name: 'weekly',
+	          value: 'weekly'
+	        }, 
+	        {
+	          name: 'fortnightly',
+	          value: 'fortnightly'
+	        }, 
+	        {
+	        	name: 'monthly',
+	        	value: 'monthly'
+	        }
+	    ];
+
+	    $scope.freq = $scope.options[0];
+
 })
